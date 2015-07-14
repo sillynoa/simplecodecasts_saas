67ee3e3a045cafe51ffa8a649083fafa798216fb
Noa Baron <sillynoa@gmail.com>
2015-07-14 00:22:08 +0000

    Fixed problem I hope

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150620223304) do
+ActiveRecord::Schema.define(version: 20150617222710) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
@@ -19,9 +19,6 @@ ActiveRecord::Schema.define(version: 20150620223304) do
     t.text     "comments"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "plans", force: true do |t|
   end
 
   create_table "users", force: true do |t|
